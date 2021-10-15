dd59e77fef2936db9b9d7609b635a10dec018d6c
bijink <kbijin528@gmail.com>
2021-10-15 15:35:09 +0530

    Update node modules

--- src/assets/Heart.jsx
+++ src/assets/Heart.jsx
@@ -14,11 +14,13 @@ export default (props) => {
          viewBox="0 0 1024 1024"
          data-aut-id="icon"
          fillRule="evenodd"
-         style={!props.quickMenu && !props.fav ? { fill: color } : { fill: 'transparent' }}
+         // style={!props.quickMenu && !props.fav ? { fill: color } : { fill: 'transparent' }}
+         style={!props.quickMenu ? { fill: color } : { fill: 'transparent' }}
          onMouseEnter={() => !props.fullHeart && changeColor('red')}
          onMouseLeave={() => changeColor('#002f34')}
          onClick={e => {
-            !props.quickMenu && !props.fav && e.stopPropagation();
+            // !props.quickMenu && !props.fav && e.stopPropagation();
+            !props.quickMenu && e.stopPropagation();
             !props.fullHeart && props.addFavList(props.product);
             props.fullHeart && props.removeFavList(props.product);
          }}
@@ -35,6 +37,6 @@ export default (props) => {
                   d="M705.941 124.121c-80.853 0-152.204 41.445-193.939 104.204-41.736-62.759-113.086-104.204-193.939-104.204-128.33 0-232.727 104.398-232.727 232.727 0 50.657 16.194 98.967 47.806 140.916l328.766 402.114h100.189l329.716-403.355c30.662-40.708 46.856-89.018 46.856-139.675 0-128.33-104.398-232.727-232.727-232.727z"
                />
          }
-      </svg>
+      </svg >
    );
 };
